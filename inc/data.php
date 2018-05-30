b0b17f445a49eac7d24e254234f20135b753b00a
Francis Rivera <rivera.francis9@gmail.com>
2018-05-30 19:15:50 -0400

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,181 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+   "title" => "Ready Player One",
+   "img" => "img/media/readyp1.png",
+   "genre" => "Action-Drama",
+   "format" => "Paperback",
+   "year" => 2011,
+   "category" => "Books",
+   "authors" => [
+   "Ernest Cline",
+   ],
+   "publisher" => "Random House",
+   "isbn" => '9782266242332'
+];
+
+$catalog[102] = [
+   "title" => "Divergent",
+   "img" => "img/media/divergent.jpg",
+   "genre" => "Dystopian Future",
+   "format" => "Paperback",
+   "year" => 2011,
+   "category" => "Books",
+   "authors" => [
+   "Veronica Roth",
+   ],
+   "publisher" => "HarperCollins",
+   "isbn" => '9780062024039'
+];
+
+$catalog[103] = [
+   "title" => "A Perfect Union of Contrary Things",
+   "img" => "img/media/apuoct.jpg",
+   "genre" => "Biography",
+   "format" => "Paperback",
+   "year" => 2016,
+   "category" => "Books",
+   "authors" => [
+   "Maynard James Keenan","Sarah Jensen",
+   ],
+   "publisher" => "Backbeat Books",
+   "isbn" => '9781495024429'
+];
+
+$catalog[104] = [
+   "title" => "Thug Kitchen: The Official Cookbook: Eat Like You Give a F*ck",
+   "img" => "img/media/thugkitchen.jpg",
+   "genre" => "Tutorials and How To",
+   "format" => "Paperback",
+   "year" => 2014,
+   "category" => "Books",
+   "authors" => [
+   "Matt Holloway","Michelle Davis",
+   ],
+   "publisher" => "Rodale Books",
+   "isbn" => '9781770894655'
+];
+
+//Movies
+$catalog[201] = [
+   "title" => "Grave Encounters",
+   "img" => "img/media/graveencounters.jpg",
+   "genre" => "Horror",
+   "format" => "DVD",
+   "year" => 2011,
+   "category" => "Movies",
+   "directors" => "John Poliquin", "Colin Minihan", "Stuart Ortiz",
+   "writers" => [
+       "The Vicious Brothers"
+
+   ],
+   "stars" => [
+       "Sean Rogerson",
+       "Ashleigh Gryzko",
+       "Juan Riedinger"
+   ]
+];
+
+$catalog[202] = [
+   "title" => "Gwai Wik",
+   "img" => "img/media/gwaiwik.jpg",
+   "genre" => "Horror",
+   "format" => "DVD",
+   "year" => 2006,
+   "category" => "Movies",
+   "directors" => "Oxide Pang Chun", "Danny Pang Phat",
+   "writers" => [
+       "Cub Chin", "Sam Lung"
+
+   ],
+   "stars" => [
+       "Angelica Lee",
+       "Lawrence Chou",
+       "Siu-Ming Lau"
+   ]
+];
+
+$catalog[203] = [
+   "title" => "Silent Hill",
+   "img" => "img/media/sh.jpg",
+   "genre" => "Horror",
+   "format" => "DVD",
+   "year" => 2006,
+   "category" => "Movies",
+   "director" => "Christophe Gans",
+   "writer" => [
+       "Roger Avary"
+
+   ],
+   "stars" => [
+       "Radha Mitchell",
+       "Sean Bean",
+       "Jodelle Ferland"
+   ]
+];
+
+$catalog[204] = [
+   "title" => "Jacob's Ladder",
+   "img" => "img/media/jacobsladder.jpg",
+   "genre" => "Horror",
+   "format" => "DVD",
+   "year" => 1990,
+   "category" => "Movies",
+   "director" => "Adrian Lyne",
+   "writers" => [
+       "Bruce Joel Rubin"
+
+   ],
+   "stars" => [
+       "Tim Robbins",
+       "Elizabeth Peña",
+       "Danny Aiello"
+   ]
+];
+
+//Music
+$catalog[301] = [
+   "title" => "V is for Vagina",
+   "img" => "img/media/puscifer.jpg",
+   "genre" => "Experimental Grunge",
+   "format" => "CD",
+   "year" => 2007,
+   "category" => "Music",
+   "artist" => "Puscifer"
+];
+
+$catalog[302] = [
+   "title" => "10,000 Days",
+   "img" => "img/media/10000days.jpg",
+   "genre" => "Psychedelic Metal",
+   "format" => "CD",
+   "year" => 2006,
+   "category" => "Music",
+   "artist" => "Tool"
+];
+
+$catalog[303] = [
+   "title" => "Mer de Noms",
+   "img" => "img/media/apc.jpg",
+   "genre" => "Alternative Rock",
+   "format" => "CD",
+   "year" => 2000,
+   "category" => "Music",
+   "artist" => "A Perfect Circle"
+];
+
+$catalog[304] = [
+   "title" => "Heritage",
+   "img" => "img/media/opeth.jpg",
+   "genre" => "Progressive Metal",
+   "format" => "CD",
+   "year" => 2011,
+   "category" => "Music",
+   "artist" => "Opeth"
+];
+
+(edited)
+
+
+?>
---
+++ inc/footer.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title><?php echo $pageTitle; ?></title>
+<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+<div class="footer">
+<div class="wrapper">
+<ul>
+<li><a href="#">Twitter</a></li>
+<li><a href="#">Facebook</a></li>
+</ul>
+<p>&copy;<?php echo date("Y"); ?>My Online Library</p>
+    </div>
+    </div>
+</body>
+</html>
